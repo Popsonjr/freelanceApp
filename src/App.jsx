d55702d300eca4d46b2e2b81349ad2a529a8a54f
Muhammad Popoola <popoolama@yahoo.com>
2023-07-01 11:55:37 +0100

    navbar completed and routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,93 @@
 import React from 'react'
 // import './App.css'
+import "./app.scss"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
 import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home'
+import Gig from './pages/gig/Gig'
+import Add from './pages/add/Add'
+import Gigs from './pages/Gigs/Gigs'
+import Login from './pages/Login/Login'
+import Message from './pages/Message/Message'
+import Messages from './pages/Messages/Messages'
+import MyGigs from './pages/MyGigs/MyGigs'
+import Orders from './pages/Orders/Orders'
+import Register from './pages/register/Register'
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />
+
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />
+
+        },
+        {
+          path: "/orders",
+          element: <Orders />
+
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />
+
+        },
+        {
+          path: "/add",
+          element: <Add />
+
+        },
+        {
+          path: "/messages",
+          element: <Messages/>
+
+        },
+        {
+          path: "/message/:id",
+          element: <Message />
+
+        },
+        {
+          path: "/register",
+          element: <Register />
+
+        },
+      ]
+    },
+  ]);
+
   return (
     <>
-      <Navbar />
+      <RouterProvider router={router} />
       
     </>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import "./Navbar.scss"
 
 const Navbar = () => {
 
     const [active, setActive] = useState(false)
+    const [open, setOpen] = useState(false)
+
+    const {pathname} = useLocation()
 
     const isActive = () => {
         window.scrollY > 0 ? setActive(true) : setActive(false)
@@ -26,48 +30,73 @@ const Navbar = () => {
     }
 
   return (
-    <div className={`navbar ${active ? 'active' : ''}`}>
+    <div className={`navbar ${active || pathname !== "/" ? 'active' : ''}`}>
         <div className="container">
             <div className="logo">
-                {/* <Link to="/"> */}
+                <Link to="/" className='link'>
                     <span className='text'>MeetUp</span>
                     <span className="dot">.</span>
-                {/* </Link> */}
+                </Link>
             </div>
             <div className="links">
                 <span>MeetUp Business</span>
                 <span>Explore</span>
                 <span>English</span>
-                <span>Sign in</span>
+                {!currentUser && <span>Sign in</span> }
                 {!currentUser?.isSeller &&<span>Become a Seller</span>}
                 {!currentUser && <button>Join</button>}
                 {currentUser && (
-                    <div className='user'>
-                        <img src='' alt="" />
+                    <div className='user' onClick={() => setOpen(!open)}>
+                        <img src='https://images.unsplash.com/photo-1516735492913-6343ed4e1f67?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=30&q=60' alt="" />
                         <span>{currentUser?.username}</span>
-                        <div className="options">
+                        { open && <div className="options">
                             {
                                 currentUser?.isSeller && (
                                     <>
-                                        <span>Gigs</span>
-                                        <span>Add New Gig</span>
+                                        <Link  className='link' to="/mygigs">Gigs</Link>
+                                        <Link className='link' to="/add">Add New Gig</Link>
                                     </>
                                 )
                             }
-                            <span>Orders</span>
-                            <span>Messages</span>
-                            <span>Logout</span>
-                        </div>
+                            <Link className='link' to="/orders">Orders</Link>
+                            <Link className='link' to="/messages">Messages</Link>
+                            <Link className='link' to="/">Logout</Link>
+                        </div>}
                     </div>
                 )}
             </div>
         </div>
-        { active && (
+        { active || pathname !== "/" && (
             <>
                 <hr />
                 <div className="menu">
-                    <span>Test</span>
-                    <span>Test2</span>
+                    <Link className='link menuLink' to="/">
+                        Graphics & Design
+                    </Link>
+                    <Link className='link menuLink' to="/">
+                        Video & Animation
+                    </Link>
+                    <Link className='link menuLink' to="/">
+                        Writing & Translation
+                    </Link>
+                    <Link className='link menuLink' to="/">
+                        AI Services
+                    </Link>
+                    <Link className='link menuLink' to="/">
+                        Digital Marketing
+                    </Link>
+                    <Link className='link menuLink' to="/">
+                        Music & Audio
+                    </Link>
+                    <Link className='link menuLink' to="/">
+                        Programming & Tech
+                    </Link>
+                    <Link className='link menuLink' to="/">
+                        Business
+                    </Link>
+                    <Link className='link menuLink' to="/">
+                        Lifestyle
+                    </Link>
                 </div>
             </>
             )
