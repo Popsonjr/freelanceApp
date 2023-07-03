c63f4aee00ac8ec7a645e879c43adc3aefa4cc8c
Muhammad Popoola <popoolama@yahoo.com>
2023-07-03 01:49:29 +0100

    slider

---
+++ src/components/catCard/CatCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./CatCard.scss"
+const CatCard = ({item}) => {
+  return (
+    <Link to="/gigs?cat=design">
+      <div className='catCard'>
+          <img src={item.img} alt={item.title} />
+          <span className='desc'>{item.description}</span>
+          <span className="title">{item.title}</span>
+      </div>
+    </Link>
+  )
+}
+
+export default CatCard
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
+import Slide from "../../components/slide/Slide"
 import "./Home.scss"
+import CatCard from '../../components/catCard/CatCard';
+import { cards } from '../../data';
+
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map(card => (
+          <CatCard item={card} key={card.id} />
+          ))}
+      </Slide>
     </div>
   )
 }
