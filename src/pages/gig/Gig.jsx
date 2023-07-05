57c26cdebd649d9419e38e56361349fc737a60f1
Muhammad Popoola <popoolama@yahoo.com>
2023-07-05 14:47:26 +0100

    gigs component

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 import "./Gig.scss"
 const Gig = () => {
   return (
-    <div>Gig</div>
+    <div className='gig'>
+      <div className="container">
+        <div className="left">
+          <span className="breadcrumbs">MEETUP > GRAPHICS & DESIGN > </span>
+          <h1>I will create AI generated art for you</h1>
+
+          <div className="user">
+            <img src="" alt="" />
+            <span>Popson Jr</span>
+            <div className="stars">
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <span>5</span>
+            </div>
+          </div>
+          <Slider>
+            <img src="" alt="" />
+          </Slider>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import GigCard from '../../components/gigCard/gigCard'
+import { gigs } from '../../data'
 import "./Gigs.scss"
 const Gigs = () => {
+
+  const[sort, setSort] = useState("sales")
+  const [open, setOpen] = useState(false)
+
+  const reSort = (type) => {
+    setSort(type)
+    setOpen(false)
+  }
+
   return (
-    <div>Gigs</div>
+    <div className='gigs'>
+      <div className="container">
+        <span className="breadcrumbs">MEETUP > GRAPHICS & DESIGN > </span>
+        <h1>AI Artists</h1>
+        <p>Explore the boundaries of art and technology with MeetUp's AI artists</p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder='min' />
+            <input type="text" placeholder='max' />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className='sortBy'>SortBy</span>
+            <span className="sortType">{sort === "sales" ? "Best Selling" : "Newest"}</span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (<div className="rightMenu">
+              {sort === "sales" ? (<span onClick={()=> reSort("createdAt")}>Newest</span>) :
+                (<span onClick={()=> reSort("sales")}>Best Selling</span>)}
+            </div>)}
+          </div>
+        </div>
+        <div className="cards">
+          {gigs.map(gig=>(
+            <GigCard key={gig.id} item={gig}/>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
