c2e00b021051df7d3103231028d816111d68999b
Muhammad Popoola <popoolama@yahoo.com>
2023-07-08 23:59:47 +0100

    seller gigs

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,8 +1,93 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./MyGigs.scss"
 const MyGigs = () => {
   return (
-    <div>MyGigs</div>
+    <div className='myGigs'>
+      <div className="container">
+        <div className="title">
+          <h1>Gigs</h1>
+          <Link to="/add">
+            <button> Add New Gig </button>
+          </Link>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Orders</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/delete.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
 
