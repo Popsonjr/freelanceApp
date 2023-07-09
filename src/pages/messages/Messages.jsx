ba7f0fbb08ee48ca010b1c5aa3abd1cac22543ec
Muhammad Popoola <popoolama@yahoo.com>
2023-07-10 00:07:18 +0100

    orders component

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,9 +1,89 @@
 import React from 'react'
-import "./Messages.scss"
-const Messages = () => {
+import { Link } from 'react-router-dom'
+import "./Orders.scss"
+const Orders = () => {
+
+  const currentUser = {
+    id: 1,
+    username: "Popson Jr",
+    isSeller: true
+  } 
   return (
-    <div>Messages</div>
+    <div className='orders'>
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+         
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          
+          
+        </table>
+      </div>
+    </div>
   )
 }
 
-export default Messages
+export default Orders
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,8 +1,88 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Orders.scss"
 const Orders = () => {
+
+  const currentUser = {
+    id: 1,
+    username: "Popson Jr",
+    isSeller: true
+  } 
   return (
-    <div>Orders</div>
+    <div className='orders'>
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+         
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className='img' src="https://fiverr-res.cloudinary.com/t_gig_cards_web_x2,q_auto,f_auto/gigs/308814022/original/2aa6bf69c0accb5ba5791fb6689973136242d47b.jpg" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className='delete' src="img/message.png" alt="" />
+            </td>
+          </tr>
+          
+          
+        </table>
+      </div>
+    </div>
   )
 }
 
